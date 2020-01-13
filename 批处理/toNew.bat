57ab6ac4df732051d64e7b4030d105850915b8a4
huazi1234 <cahczh@163.com>
2020-01-13 13:54:31 +0800

    Add files via upload

---
+++ 批处理/toNew.bat
@@ -0,0 +1,4 @@
+
+del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\DOC_ZhongKong - Copy.lnk"
+copy /y "%userprofile%\Desktop\start - Shortcut.lnk" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy /y "%userprofile%\Desktop\Server.lnk" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
---
+++ 批处理/toold.bat
@@ -0,0 +1,3 @@
+del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start - Shortcut.lnk"
+del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Server.lnk"
+copy /y "%userprofile%\Desktop\DOC_ZhongKong - Copy.lnk" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
